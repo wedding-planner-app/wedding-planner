9e8a48ebe1eedc4cb64936b20dac533bded829f9
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-22 12:25:49 -0500

    added server setup default

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./controllers');
+const app = express();
+const PORT = process.env.PORT || 3001;
+var db = require('./models');
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Start the API server
+// ADD SEQUELIZE HERE TO CONNECT TO YOUR DB
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
+});
