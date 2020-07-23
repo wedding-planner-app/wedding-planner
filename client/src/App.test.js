61e75ad8a7d2ea83d2318b0336ff03c12f4c0b9b
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-22 22:56:40 -0500

    added comment to the test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+// sample test scenario
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
