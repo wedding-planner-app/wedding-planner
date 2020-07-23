452a24957b92e5d6c9a55334c2606d94e1e1feb0
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-22 23:16:31 -0500

    create the shema sql file

---
+++ scripts/schema.sql
@@ -0,0 +1,4 @@
+-- Drops the wedding_planner_db if it exists currently --
+DROP DATABASE IF EXISTS wedding_planner_db;
+-- Creates the "wedding_planner_db" database --
+CREATE DATABASE wedding_planner_db;
