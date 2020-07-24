cdbbdc52f47cbcc4223ea22c71d4a6c11d9d3ac6
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-24 14:30:47 -0500

    added Navbar to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Form from './components/Component';
+import Navbar from './components/Navbar';
 
 function App() {
-  return null;
+  return <Navbar />;
 }
 
 export default App;
