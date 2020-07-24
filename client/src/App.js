cb09c058b9ef4c81fa29e341de18d86aac9984ff
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-24 18:16:47 -0500

    added dependecies to the json file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import NavigationBar from './components/NavigationBar';
+import EventsPage from './pages/Events';
+import LoginPage from './pages/Login';
+import LogoutPage from './pages/Logout';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-      </div>
+      <NavigationBar />
+      <Container>
+        <Route exact path="/" component={EventsPage} />
+        <Route exact path="/events" component={EventsPage} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/logout" component={LogoutPage} />
+      </Container>
     </Router>
   );
 }
---
+++ client/src/components/NavigationBar/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Nav, Navbar, Image } from 'react-bootstrap';
+import './style.css';
+import logo from './logo.png';
+
+/**
+ *  Navbar Component using {Link}
+ */
+function NavigationBar() {
+  return (
+    <Navbar bg="light" shadow="lg" expand="lg">
+      <Navbar.Brand href="/events">
+        <Image src={logo} className="custom-img" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse>
+        <Nav className="ml-auto color-link">
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/signup">Sign up</Nav.Link>
+          <Nav.Link href="/events">Events</Nav.Link>
+          <Nav.Link href="/profile">Profile</Nav.Link>
+          <Nav.Link href="/logout">Logout</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
