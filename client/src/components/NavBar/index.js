106c8e912b5a72dd096ed477c8a4d0da555d7e63
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-24 15:12:40 -0500

    added sign up nav item with link

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -10,12 +10,15 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Wedding Planner
+        Wedding Planner Logo
       </Link>
       <div>
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link to="/login">Log In</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/signup">Sign Up</Link>
           </li>
         </ul>
       </div>
