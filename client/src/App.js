797ecd3f81dede8865418acef8c6aa96b101ca9c
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-24 15:19:45 -0500

    added Router to App js

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
