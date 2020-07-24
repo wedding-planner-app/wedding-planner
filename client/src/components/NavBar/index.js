33f3599892a21eb3054ddba2354099b0be0c96cf
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-24 14:28:56 -0500

    added comment

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 
+/**
+ *  Navbar Component
+ *  Description:
+ */
 function Navbar() {
   return null;
 }
