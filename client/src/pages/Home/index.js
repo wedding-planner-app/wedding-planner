21f992fedff3860e398cf2df661050e04003ac07
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-25 08:56:30 -0500

    added color to the message component

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -6,8 +6,22 @@ import Message from '../../components/Message';
 const HomePage = () => {
   return (
     <Container fluid className="p-0">
-      <CarouselPage />
-      <Message />
+      <Row>
+        <Col>
+          <CarouselPage />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Message />
+        </Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+        <Col>3 of 4</Col>
+      </Row>
     </Container>
   );
 };
