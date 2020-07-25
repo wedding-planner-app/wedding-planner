a8b37bfb67a3b9444d63f84839d0f66879388f59
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-25 10:44:24 -0500

    removed unnecesary classes from home page

--- client/src/components/InfoCard/index.js
+++ client/src/components/InfoCard/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import venues from './imgs/venues.jpg';
 import './style.css';
 
-const InfoCard = () => {
+const InfoCard = (props) => {
   return (
-    <Card
-      style={{ width: '20rem' }}
-      className="ml-5 card-hover-style"
-    >
-      <Card.Img variant="top" src={venues} />
-      <Card.Body>
-        <Card.Title className="cards-component-title">
-          Venues
-        </Card.Title>
-        <Card.Text className="cards-component-text">
-          Venues Summary Text
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div className="wraper mb-5">
+      <Card
+        style={{ width: '20rem' }}
+        className="ml-5 card-hover-style"
+      >
+        <Card.Img variant="top" src={props.img} />
+        <Card.Body>
+          <Card.Title className="cards-component-title">
+            {props.title}
+          </Card.Title>
+          <Card.Text className="cards-component-text">
+            {props.summary}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
