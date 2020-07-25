b36dc8ae39762ea2dfb73aed0831b5dfbc1e497d
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-24 21:14:35 -0500

    added footer JSX to index.js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Nav, Navbar, Image } from 'react-bootstrap';
-import './style.css';
 
+function Footer() {
+    return
+    <div className = "footer">
+        <p>Project 2 - Group 1</p>
+    </div>
+}
 
-
+export default Footer;
