511d9d524a8d6564e087e9e7e4116dd39c68bc26
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-24 21:47:58 -0500

    fixed footer styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './style.css';
+import favicon from './favicon.ico';
 
 function Footer() {
   return (
     <div className="footer">
+      <img src={favicon} className="img-padding" />
       <h6> &copy;2020 Project 2 - Group 1</h6>
+      <img src={favicon} className="img-padding" />
     </div>
   );
 }
