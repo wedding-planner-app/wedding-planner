9fa374497cdc50344f85c1450505cbde7af9bbe5
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-25 13:33:22 -0500

    added basic email component into index.js in Email component folder

---
+++ client/src/components/Forms/Email/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Email() {
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
+
+export default Email;
