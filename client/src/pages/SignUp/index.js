cab833a1be914ade392d288a103093ade519e5b8
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-25 22:53:13 -0500

    added signup page component

--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Card, Col, Row, Container } from 'react-bootstrap';
+import user from './user.png';
+import './style.css';
+import BtnComponent from '../../components/Button';
+import Email from '../../components/Email';
+import Password from '../../components/Password';
 
 const SignUp = () => {
-  return <h1>SignUp page</h1>;
+  return (
+    <Container fluid className="pt-5">
+      <Row>
+        <Col className="col-6">
+          <Card className="bgLoginComponent custom-style-login bg-light">
+            <Card.Header className="text-center">
+              <Card.Title className="login-title-style">
+                <h3>SignUp</h3>
+              </Card.Title>
+            </Card.Header>
+            <Col className="custom pt-3">
+              <Card.Img
+                className="login-img-style"
+                variant="top"
+                src={user}
+              />
+            </Col>
+            <Email />
+            <Password />
+            <BtnComponent />
+          </Card>
+        </Col>
+        <Col className="col-3"></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default SignUp;
