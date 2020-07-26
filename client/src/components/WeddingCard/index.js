de30d8301f865f9c8e22ebeda0f1f5d5f29e7fd3
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-25 20:23:51 -0500

    tranform the wedding card in a reusable component

--- client/src/components/WeddingCard/index.js
+++ client/src/components/WeddingCard/index.js
@@ -1,53 +1,21 @@
 import React from 'react';
-import { CardDeck, Card } from 'react-bootstrap';
-import logo from './logo.png';
+import { CardDeck, Card, Button } from 'react-bootstrap';
 
-const WeddingCard = () => {
+const WeddingCard = (props) => {
   return (
-    <CardDeck className="mt-5 pt-5 mr-5 ml-5">
-      <Card>
-        <Card.Img variant="top" src={logo} />
-        <Card.Body>
-          <Card.Title>Events</Card.Title>
-          <Card.Text>Wedding Data</Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">from wedding team</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in
-            to additional content.{' '}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">
-            Last updated 3 mins ago
-          </small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a
-            natural lead-in to additional content. This card has even
-            longer content than the first to show that equal height
-            action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">
-            Last updated 3 mins ago
-          </small>
-        </Card.Footer>
-      </Card>
-    </CardDeck>
+    <Card style={{ height: '30erm' }}>
+      <Card.Img variant="top" src={props.img} />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>{props.data}</Card.Text>
+        <Button />
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">
+          update by {props.username}
+        </small>
+      </Card.Footer>
+    </Card>
   );
 };
 
