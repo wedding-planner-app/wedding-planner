5b0dc88f561454074a160b4ae2371ce1363c24cb
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-26 19:24:52 -0500

    deleted no neccesary text component

--- client/src/components/Text/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
-import './style.css';
-
-const TextComponent = () => {
-  return <Form.Control type="text" placeholder="Normal text" />;
-};
-
-export default TextComponent;
