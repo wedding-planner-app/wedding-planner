6e35bc98a518b6cf7d0271b2126e163e97cb453c
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-28 14:25:38 -0500

    added User model to db

--- models/user.js
+++ models/user.js
@@ -1,7 +1,31 @@
 // User model placeholder
 
 module.exports = function (sequelize, DataTypes) {
-  var User = sequelize.define();
-  // TODOs
+  const User = sequelize.define('user', {
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+      validate: {
+        // checks for email format (yst@gmail.com)
+        isEmail: {
+          msg:
+            'Please enter a valid email format: example@gmail.com ',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING(64),
+      // added validation to password (must match this RegExp)
+      validate: {
+        is: /^[0-9a-f]{64}$/i,
+      },
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
+
   return User;
 };
