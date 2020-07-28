b09b619d73d5e14fb23cd102cef092284be81432
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-28 10:45:17 -0500

    added title , description, date wedding table columns with minor validation

--- models/wedding.js
+++ models/wedding.js
@@ -1,5 +1,23 @@
 module.exports = function (sequelize, Datatypes) {
-  var Wedding = sequelize.define('Wedding', {});
+  // creating table wedding
+  var Wedding = sequelize.define('Wedding', {
+    // added validation allowNull => false
+    title: {
+      // title type STRING (Default length 255)
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      // added TEXT datatypes because is a long text (An unlimited length text column)
+      type: Datatypes.TEXT,
+      allowNull: false,
+    },
+    date: {
+      type: Datatypes.DATE,
+      allowNull: false,
+    },
+    time: {},
+  });
 
-  return '';
+  return Wedding;
 };
