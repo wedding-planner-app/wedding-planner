f5857638b0be3a86f801d70ce39c01d2fc4bdb98
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-27 22:06:45 -0500

    defined the venues model

--- models/venues.js
+++ models/venues.js
@@ -1,7 +1,13 @@
-// Venue model for database
+// User model placeholde
 
 module.exports = function (sequelize, DataTypes) {
-    var Venues = sequelize.define();
-    // TODOs
-    return Venues;
-  };
+  var Venues = sequelize.define('Venues', {
+    name: DataTypes.STRING,
+    street: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    zipcode: DataTypes.STRING,
+  });
+  // Venues model
+  return Venues;
+};
