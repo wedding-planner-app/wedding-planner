3a04d536cdef0ec4d4e429a712856ffcbbced170
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-28 18:24:42 -0500

    added parameters for name and street variable, including validation and pseudocode

--- models/venue.js
+++ models/venue.js
@@ -2,8 +2,26 @@
 module.exports = function (sequelize, DataTypes) {
   var Venue = sequelize.define('Venue', {
     // Creating columns to store the required data in the venue table
-    name: DataTypes.STRING,
-    street: DataTypes.STRING,
+    // Code to create name column in Venue table
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Must entere a venue name',
+        },
+      },
+    },
+    // Code to create street column in Venue table
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Must enter a street for the venue',
+        },
+      },
+    },
     city: DataTypes.STRING,
     state: DataTypes.STRING,
     zipCode: DataTypes.STRING,
