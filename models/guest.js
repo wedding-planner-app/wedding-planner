c89c3a5ad178f9e23a51d78299adf8cfdd12c652
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-28 19:52:12 -0500

    added a additional parameter details for the fiels

--- models/guest.js
+++ models/guest.js
@@ -1,12 +1,38 @@
 module.exports = function (sequelize, DataTypes) {
   var Guest = sequelize.define('Guest', {
     // Adds NAME data type
-    name:{},
-   // Adds EMAIL data type
-    email:{},
+    name: {
+      // Name type STRING
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Must to enter guest name',
+        },
+      },
+    },
+    // Adds EMAIL data type
+    email: {
+      // Email type STRING
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Must to enter quest email',
+        },
+      },
+    },
     // Adds phone data type
-    phone:{},
-
+    phone: {
+      // Phone type STRING
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Must to enter quest phone number',
+        },
+      },
+    },
   });
 
   // When an Wedding is deleted, also delete any associated Venue
