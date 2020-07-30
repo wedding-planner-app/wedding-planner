8a9b023904b35765d6e204cd1ff99a206c5fc713
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-29 22:00:44 -0500

    fixed responsive spacing in profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FirstName from '../../components/FirstName';
 import LastName from '../../components/LastName';
 import Email from '../../components/Email';
+
 import { Card, Container, Row, Col, Button } from 'react-bootstrap';
 import user from './user.png';
 import './style.css';
@@ -12,8 +13,8 @@ const ProfilePage = () => {
   return (
     <Container fluid className="pt-5">
       <Row>
-        <Col className="col-3"></Col>
-        <Col className="col-6">
+        <Col xs="2" md="3" lg="3"></Col>
+        <Col xs="8" md="6" lg="6">
           <Card className="bgProfileComponent custom-style-profile bg-light">
             <Card.Header>
               <Card.Title className="profile-title-style">
@@ -31,14 +32,16 @@ const ProfilePage = () => {
               <FirstName className="custom" />
               <LastName />
               <Email />
+              <Col className="center-btn">
                 <Button className="btnColor">
                   <FontAwesomeIcon icon={faPlus} />
                   Create New Reservation
                 </Button>
+              </Col>
             </Card.Body>
           </Card>
         </Col>
-        <Col className="col-3"></Col>
+        <Col xs="2" md="6" lg="3"></Col>
       </Row>
     </Container>
   );
