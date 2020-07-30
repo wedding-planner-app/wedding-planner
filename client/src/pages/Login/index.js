8e6ce849f39e6357c9acfa691456a43046dfcf9a
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-30 18:40:59 -0500

    added login function working with error when the password is wrong

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,26 +1,63 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Email from '../../components/Email';
 import Password from '../../components/Password';
 import BtnComponent from '../../components/Button';
 import { Card, Container, Row, Col } from 'react-bootstrap';
 import user from './user.png';
 import './style.css';
+import { Redirect } from 'react-router-dom';
 
 // function component using {useState} Hook
 export default function LoginPage() {
   // added const `state` for email and password
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [userId, setUserId] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState();
 
   // validate email and password is not null
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
 
-  // prevent default
-  function handleSubmit(event) {
-    event.preventDefault();
+  async function callLoginAPI() {
+    var data = JSON.stringify({
+      email: email,
+      password: password,
+    });
+
+    var config = {
+      method: 'post',
+      url: '/api/user/login',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+
+    try {
+      let response = await axios(config);
+      setEmail('');
+      setPassword('');
+      setUserId(response.data.id);
+      setIsLoggedIn(true);
+      console.log(response.data);
+    } catch (e) {
+      setPassword('');
+      setErrorMessage('Invalid login');
+      console.log(e);
+    }
   }
+
+  // prevent default
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await callLoginAPI();
+  }
+
+  if (isLoggedIn) return <Redirect to="/events" />;
 
   return (
     <Container fluid className="pt-5">
@@ -55,6 +92,7 @@ export default function LoginPage() {
                 </Col>
               </form>
             </Card.Body>
+            <p hidden={!errorMessage}>{errorMessage}</p>
           </Card>
         </Col>
       </Row>
