5c8c10129badbfcaaa9ef2db339b51d52070094f
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-30 11:54:41 -0500

    removed Form wrap in email component

--- client/src/components/Email/index.js
+++ client/src/components/Email/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import './style.css';
 
-function Email() {
+function Email(props) {
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label className="text-style">Email Address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-    </Form>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label className="text-style">Email Address</Form.Label>
+      <Form.Control
+        type="email"
+        placeholder="Enter email"
+        value={props.value}
+        onChange={props.onChange}
+      />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
   );
 }
 
