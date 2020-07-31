bd8fa556db5569233fdb3c4fd762f2da4e6767d8
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-07-31 14:14:27 -0500

    Develop Guests API

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
 const weddingRoutes = require('./wedding');
+const guestsRoutes = require('./guests.js');
 
 // api routes
 router.use('/user', userRoutes);
 router.use('/weddings', weddingRoutes);
+router.use('/guests', guestsRoutes);
 
 module.exports = router;
