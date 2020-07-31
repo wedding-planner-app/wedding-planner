8a084ffa629258a31e4ef74c6fe7d1e8994258d8
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-30 21:08:56 -0500

    added venue route to the index js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./user');
 const weddingRoutes = require('./wedding');
 const invitationRoutes = require('./invitation.js');
+const venueRoutes = require('./venue.js');
 
 // api routes
 router.use('/user', userRoutes);
 router.use('/weddings', weddingRoutes);
 router.use('/invitation', invitationRoutes);
+router.use('/venue', venueRoutes);
 
 module.exports = router;
