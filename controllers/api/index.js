f108480727a59019fe32af91c93e80c74727ee14
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-30 21:00:01 -0500

    removed vscode folder and added media API files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./user');
 const weddingRoutes = require('./wedding');
 const invitationRoutes = require('./invitation.js');
+const mediaRoutes = require('./media.js');
 
 // api routes
 router.use('/user', userRoutes);
 router.use('/weddings', weddingRoutes);
 router.use('/invitation', invitationRoutes);
+router.use('/media', mediaRoutes)
 
 module.exports = router;
