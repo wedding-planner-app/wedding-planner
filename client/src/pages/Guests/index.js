03d0066f9e23161a2a1b41ac007ad3f9bdeb3586
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-01 18:10:13 -0500

    running more tests

--- client/src/pages/Guests/index.js
+++ client/src/pages/Guests/index.js
@@ -28,7 +28,6 @@ const GuestsPage = () => {
         <Button variant="outline-success">ADD</Button>{' '}
         <Button variant="outline-primary">EDIT</Button>{' '}
         <Button variant="outline-danger">REMOVE</Button>{' '}
-        <ModalAlert />
       </div>
 
       <div>
