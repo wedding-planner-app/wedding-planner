928fea41301cc398d53667941e569b1867c6370b
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-01 13:55:36 -0500

    Show loading when login

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,45 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
 import EventsPage from './pages/Events';
-import LoginPage from './pages/Login';
 import ProfilePage from './pages/Profile';
-import LogoutPage from './pages/Logout';
-import SignUp from './pages/SignUp';
 import HomePage from './pages/Home';
 import VenuesPage from './pages/Venue';
 import RSVPPage from './pages/Invitation-RSVP';
 import NewReservationPage from './pages/NewReservation';
+import Loading from './components/Loading';
 import GuestsPage from './pages/Guests';
+import { useAuth0 } from '@auth0/auth0-react';
 
+const App = () => {
+  const { isLoading } = useAuth0();
 
+  if (isLoading) {
+    return <Loading />;
+  }
 
-function App() {
   return (
     <Router>
       <NavigationBar />
       <Route exact path="/" component={HomePage} />
       <Route exact path="/events" component={EventsPage} />
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/login" component={LoginPage} />
       <Route exact path="/profile" component={ProfilePage} />
-      <Route exact path="/logout" component={LogoutPage} />
       <Route exact path="/venues" component={VenuesPage} />
       <Route exact path="/RSVP" component={RSVPPage} />
-      <Route exact path="/NewReservation" component={NewReservationPage} />
+      <Route
+        exact
+        path="/NewReservation"
+        component={NewReservationPage}
+      />
       <Route exact path="/guests" component={GuestsPage} />
       <Footer />
     </Router>
   );
-}
+};
 
 export default App;
