33e798c5a3a5ddb6fb41f6a3b9a0e0728209c6b0
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-01 12:39:16 -0500

    removed index style css

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Auth0ProviderWithHistory from './auth0-provider-with-history';
 
-// import './index.css';
-
 ReactDOM.render(
   <Router>
     <Auth0ProviderWithHistory>
