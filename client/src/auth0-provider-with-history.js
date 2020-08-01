b940439c97c8fb7dee4d856a4905746b8a5e9c51
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-01 11:30:25 -0500

    added const to the provider components to manage react and auth0

--- client/src/auth0-provider-with-history.js
+++ client/src/auth0-provider-with-history.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const Auth0ProviderWithHistory = ({ children }) => {};
+const Auth0ProviderWithHistory = ({ children }) => {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  const history = useHistory();
+
+  const onRedirectCallback = (appState) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
+};
 
 export default Auth0ProviderWithHistory;
