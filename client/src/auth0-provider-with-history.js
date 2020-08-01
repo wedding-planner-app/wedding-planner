cea738bd56650b1cc06ef7ba93ee65ad6ec60dfd
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-01 11:29:26 -0500

    populate the file with the Auth0ProviderWithHistory component

--- client/src/auth0-provider-with-history.js
+++ client/src/auth0-provider-with-history.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
+
+const Auth0ProviderWithHistory = ({ children }) => {};
+
+export default Auth0ProviderWithHistory;
