f791c9558f9778f74dd25bb5c8d60ddc2ddbbe07
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-01 15:04:48 -0500

    fixed row col profile

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -22,22 +22,25 @@ const ProfilePage = () => {
             <Card.Header className="profile-title-style bg-light">
               <h3>Profile</h3>
             </Card.Header>
-            <Card.Body>
-              <Row>
-                <Col className="center">
-                  <Card.Img
-                    className="profile-img-style"
-                    variant="top"
-                    src={picture}
-                  />
-                </Col>
-              </Row>
-              <Row className="align-items-center">
-                <Col className="m-auto">
-                  <h2>{name}</h2>
-                  <p className="lead text-muted">{email}</p>
-                </Col>
-              </Row>
+            <Card.Body className="d-flex flex-column">
+              <Col className="center">
+                <Card.Img
+                  className="profile-img-style"
+                  variant="top"
+                  src={picture}
+                />
+              </Col>
+              <Col className="text-style">
+                <h5>{name}</h5>
+                <a
+                  className="head text-muted"
+                  target="_blank"
+                  rel="email"
+                  href="mailto:{email}"
+                >
+                  {email}
+                </a>
+              </Col>
             </Card.Body>
           </Card>
         </Col>
