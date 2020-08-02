ec50a546a7a3ae999d900f8ded50b7b0bae80041
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-01 23:06:11 -0500

    change logout to icon

--- client/src/components/LogoutLink/index.js
+++ client/src/components/LogoutLink/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const LogoutLink = () => {
   const { logout } = useAuth0();
@@ -12,7 +14,7 @@ const LogoutLink = () => {
         })
       }
     >
-      Log Out
+      <FontAwesomeIcon icon={faSignOutAlt} size="lg" />
     </Nav.Link>
   );
 };
