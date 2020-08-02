21986da5b7b7a141e6e1d8398fa7abf5d5fa977f
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-02 00:21:50 -0500

    added OverlayTrigger to nav

--- client/src/components/LogoutLink/index.js
+++ client/src/components/LogoutLink/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Nav } from 'react-bootstrap';
+import { Nav, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const LogoutLink = () => {
   const { logout } = useAuth0();
   return (
-    <Nav.Link
-      onClick={() =>
-        logout({
-          returnTo: window.location.origin,
-        })
-      }
+    <OverlayTrigger
+      placement="bottom"
+      overlay={<Tooltip id="tooltip-logout">Sign Out</Tooltip>}
     >
-      <FontAwesomeIcon icon={faSignOutAlt} size="lg" />
-    </Nav.Link>
+      <Nav.Link
+        onClick={() =>
+          logout({
+            returnTo: window.location.origin,
+          })
+        }
+      >
+        <FontAwesomeIcon icon={faSignOutAlt} size="lg" />
+      </Nav.Link>
+    </OverlayTrigger>
   );
 };
 
