de41f3ab859490fe9adb613ce3fdc0d171e397a5
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-01 21:42:41 -0500

    finally got modals to work

--- client/src/pages/Guests/index.js
+++ client/src/pages/Guests/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Container, Row, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Button, Modal } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
 import SearchTable from '../../components/SearchTable';
+import InputText from '../../components/InputText';
 import './style.css';
-import ModalAlert from '../../components/ModalAlert';
 
 const GuestsPage = () => {
+  const [addShow, setAddShow] = useState(false);
+  const [editShow, setEditShow] = useState(false);
+  const [deleteShow, setDeleteShow] = useState(false);
+
   const guestListData = [
     {
       guestNameFirst: 'John',
@@ -24,10 +28,118 @@ const GuestsPage = () => {
   return (
     <Container>
       <div className="row">
-        <Button variant="outline-success">ADD</Button>{' '}
-        <Button variant="outline-primary">EDIT</Button>{' '}
-        <Button variant="outline-danger">REMOVE</Button>{' '}
-        <ModalAlert />
+        {/* Add Guest Button */}
+        <Button
+          variant="outline-success"
+          onClick={() => setAddShow(true)}
+        >
+          ADD
+        </Button>{' '}
+        {/* Modal alert to add */}
+        <Modal show={addShow} onHide={() => setAddShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add New Guest</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <InputText style="vertical-align" name="First Name" />
+            <InputText style="vertical-align" name="Last Name" />
+            <InputText style="vertical-align" name="Email" />
+            <InputText style="vertical-align" name="Phone Number" />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() => setAddShow(false)}
+            >
+              Close
+            </Button>
+            <Button
+              variant="primary"
+              onClick={() => setAddShow(false)}
+            >
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {/* Edit Guest Info Button */}
+        <Button
+          variant="outline-primary"
+          onClick={() => setEditShow(true)}
+        >
+          EDIT
+        </Button>{' '}
+        {/* Modal alert to edit */}
+        <Modal show={editShow} onHide={() => setEditShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Guest Information</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <InputText
+              style="vertical-align"
+              placeholder="existing guest info here"
+              name="First Name"
+            />
+            <InputText
+              style="vertical-align"
+              placeholder="existing guest info here"
+              name="Last Name"
+            />
+            <InputText
+              style="vertical-align"
+              placeholder="existing guest info here"
+              name="Email"
+            />
+            <InputText
+              style="vertical-align"
+              placeholder="existing guest info here"
+              name="Phone Number"
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() => setEditShow(false)}
+            >
+              Close
+            </Button>
+            <Button
+              variant="primary"
+              onClick={() => setEditShow(false)}
+            >
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {/* Delete Guest Button */}
+        <Button
+          variant="outline-danger"
+          onClick={() => setDeleteShow(true)}
+        >
+          REMOVE
+        </Button>{' '}
+        {/* Modal alert to delete*/}
+        <Modal show={deleteShow} onHide={() => setDeleteShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Delete Entry</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to delete this entry?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() => setDeleteShow(false)}
+            >
+              Do Not Delete
+            </Button>
+            <Button
+              variant="primary"
+              onClick={() => setDeleteShow(false)}
+            >
+              Yes Delete Entry
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
 
       <div>
