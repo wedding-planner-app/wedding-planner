9e506d518093f5c5ad7fe40f11b05c658abd4784
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-03 12:23:46 -0500

    Secure guests API

--- controllers/api/guests.js
+++ controllers/api/guests.js
@@ -1,19 +1,48 @@
 const router = require('express').Router();
 var db = require('../../models');
+const jwt_decode = require('jwt-decode');
+
+const getEmail = (token) => {
+  console.log(token);
+  decoded = jwt_decode(token);
+  return decoded[
+    'https://wedding-planner-platform.herokuapp.com/email'
+  ];
+};
 
 // get all guests **
 router.get('/', function (req, res) {
-  db.Guest.findAll({}).then(function (dbGuests) {
+  let email = getEmail(req.headers.authorization);
+
+  db.Guest.findAll({
+    where: {
+      WeddingId: req.query.eventid,
+    },
+    include: [
+      {
+        model: db.Wedding,
+        where: { user_email: email }, // enforces wedding belongs to user email
+      },
+    ],
+  }).then(function (dbGuests) {
     res.json(dbGuests);
   });
 });
 
 // get guest by id **
 router.get('/:id', function (req, res) {
+  let email = getEmail(req.headers.authorization);
   db.Guest.findOne({
     where: {
       id: req.params.id,
+      WeddingId: req.query.eventid,
     },
+    include: [
+      {
+        model: db.Wedding,
+        where: { user_email: email }, // enforces wedding belongs to user email
+      },
+    ],
   }).then(function (dbGuestById) {
     res.json(dbGuestById);
   });
@@ -25,6 +54,7 @@ router.post('/', function (req, res) {
     name: req.body.name,
     email: req.body.email,
     phone: req.body.phone,
+    WeddingId: req.query.eventid,
   }).then(function (dbCreateGuest) {
     res.json(dbCreateGuest);
   });
@@ -41,6 +71,7 @@ router.put('/:id', function (req, res) {
     {
       where: {
         id: req.params.id,
+        WeddingId: req.query.eventid,
       },
     },
   ).then(function (dbUpdateGuestById) {
@@ -53,6 +84,7 @@ router.delete('/:id', function (req, res) {
   db.Guest.destroy({
     where: {
       id: req.params.id,
+      WeddingId: req.query.eventid,
     },
   }).then(function (dbGuestDelete) {
     res.json(dbGuestDelete);
