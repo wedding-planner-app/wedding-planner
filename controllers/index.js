bc0789f9d0268c8001e01b024472bf1d42d4f723
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-03 11:39:03 -0500

    temporarily dissable auth0 on apis

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,12 @@ const jwksRsa = require('jwks-rsa');
 const audience = process.env.AUTH0_AUDIENCE;
 const issuer = process.env.AUTH0_ISSUER;
 
+if (!issuer || !audience) {
+  throw new Error(
+    'Please make sure that .env is in place and populated',
+  );
+}
+
 // setup jwt with auth0
 const jwtCheck = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -21,7 +27,8 @@ const jwtCheck = jwt({
 });
 
 // API Routes will start '/api'
-router.use('/api', jwtCheck, apiRoutes);
+router.use('/api', apiRoutes);
+// router.use('/api', jwtCheck, apiRoutes);
 
 // If no API routes are hit, send the React app will display the homepage
 router.use(function (req, res) {
