4edf656e31c3fa16a9096991f1c59eec4d877329
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-03 12:10:30 -0500

    Implemented venue search frontend functionality

--- client/src/components/VenuesPageComponent/index.js
+++ client/src/components/VenuesPageComponent/index.js
@@ -1,26 +1,70 @@
-import axios from 'axios';
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+  InputGroup,
+  FormControl,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
 import WeddingCard from '../WeddingCard';
 import PlaceholderImage from './placeholder_venue.png';
-import InputText from '../InputText';
 import BtnComponent from '../Button';
 import './style.css';
 
 class VenuesPageComponent extends Component {
   constructor() {
     super();
+
+    // Initialize a reference to the search text input, so we can extract its value later.
+    this.searchInput = React.createRef();
+
+    // State will hold list of venues as they arrive from the search API.
+    // isLoading is a bool to show placeholder text while data is being loaded.
     this.state = {
       venueList: [],
+      isLoading: false,
     };
   }
 
+  // When the component starts up, pre-populate venue data matching 'convention' search.
   componentDidMount() {
-    fetch(window.location.origin + '/api/venue/search?name=Convention')
-      .then((res) => res.json())
-      .then((res) => this.setState({ venueList: res.data }))
-      .catch((err) => console.log(err));
+    this.searchVenues('Convention');
   }
+
+  // Method to call venue search API endpoint
+  searchVenues = (name) => {
+    // Do not allow the user to search for a venue if the search text input is empty.
+    if (!name) {
+      alert('Please enter a venue name to search.');
+      return;
+    }
+
+    // Toggle the loading bool to show the placeholder text.
+    this.setState({ isLoading: true });
+
+    fetch(window.location.origin + '/api/venue/search?name=' + name)
+      .then((res) => res.json())
+      .then((res) =>
+        this.setState({
+          venueList: res.data,
+          isLoading: false,
+        }),
+      )
+      .catch((err) => console.log(err));
+  };
+
+  // When user hits enter while still in text input focus, trigger venue search
+  onKeyPress = (event) => {
+    if (event.charCode == 13) {
+      this.searchVenues(this.searchInput.current.value);
+    }
+  };
+
+  // When user clicks search button, trigger venue search
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.searchVenues(this.searchInput.current.value);
+  };
 
   render() {
     return (
@@ -29,27 +73,49 @@ class VenuesPageComponent extends Component {
         <h1 className="title-style">Search for Venues</h1>
         <Row className="d-flex flex-column flex-md-row vertical-align">
           <Col className="col-sm-12 col-lg-6">
-            <InputText
-              style="p5 mb-3 vertical-align"
-              name="Search by venue name"
-            />
+            <InputGroup className="p5 mb-3 vertical-align">
+              <InputGroup.Prepend>
+                <InputGroup.Text>
+                  Search by venue name
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+                ref={this.searchInput}
+                onKeyPress={this.onKeyPress}
+              />
+            </InputGroup>
           </Col>
           <Col className="col-sm-12 col-lg-6 mb-3">
-            <BtnComponent name="Search" />
+            <BtnComponent
+              name="Search"
+              onClick={this.handleFormSubmit}
+            />
           </Col>
         </Row>
         <br></br>
         {/* Venue card components */}
         <Row>
-          {this.state.venueList.map((venue) => (
+          {this.state.isLoading ? (
             <Col className="col-6 mb-3">
-              <WeddingCard
-                img={(venue.photo === 'Sorry, no photo available') ? PlaceholderImage : venue.photo}
-                name={venue.name}
-                username={venue.address}
-              />
+              <h1 className="title-style">Loading...</h1>
             </Col>
-          ))}
+          ) : (
+            this.state.venueList.map((venue) => (
+              <Col className="col-6 mb-3">
+                <WeddingCard
+                  img={
+                    venue.photo === 'Sorry, no photo available'
+                      ? PlaceholderImage
+                      : venue.photo
+                  }
+                  name={venue.name}
+                  username={venue.address}
+                />
+              </Col>
+            ))
+          )}
         </Row>
       </Container>
     );
