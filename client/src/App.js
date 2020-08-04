086e916ce43d31474f66ee26fc49809ca32a86e5
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-04 08:46:59 -0500

    Update routes to be event specific

--- client/src/App.js
+++ client/src/App.js
@@ -27,15 +27,31 @@ const App = () => {
       <Route exact path="/" component={HomePage} />
       <PrivateRoute exact path="/events" component={EventsPage} />
       <PrivateRoute exact path="/profile" component={ProfilePage} />
-      <PrivateRoute exact path="/venues" component={VenuesPage} />
-      <Route exact path="/RSVP" component={RSVPPage} />
+      <PrivateRoute
+        exact
+        path="/events/:eventId/venue"
+        component={VenuesPage}
+      />
+      <Route
+        exact
+        path="/events/:eventId/guests/:guestId/RSVP"
+        component={RSVPPage}
+      />
       <PrivateRoute
         exact
         path="/events/new"
         component={NewReservationPage}
       />
-      <PrivateRoute exact path="/guests" component={GuestsPage} />
-      <PrivateRoute exact path="/media" component={MediaPage} />
+      <PrivateRoute
+        exact
+        path="/events/:eventId/guests"
+        component={GuestsPage}
+      />
+      <PrivateRoute
+        exact
+        path="/events/:eventId/media"
+        component={MediaPage}
+      />
       <Footer />
     </Router>
   );
