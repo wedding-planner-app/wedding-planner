31304fc2607f054436656bff88aa9ffe4907596e
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-03 20:48:48 -0500

    added changes from class

--- controllers/api/venue-images.js
+++ controllers/api/venue-images.js
@@ -5,29 +5,64 @@ const { restart } = require('nodemon');
 
 // get venue image from Google Place API
 router.get('/image', function (req, res) {
+  // console/log() to test the image route
+  console.log('hit the image route');
   // photo reference id will be stored in a query param named 'id'.
   var imageId = req.query.id;
 
-  var options = {
-    method: 'GET',
+  var axios = require('axios');
+
+  var config = {
+    method: 'get',
     url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${imageId}&key=${process.env.API_KEY}`,
     headers: {},
   };
 
-  // override requests to return raw binary Buffers instead of string data
-  var requestBuffer = require('request').defaults({ encoding: null });
+  axios(config)
+    .then(function (response) {
+      console.log('Response pulled from google API');
+      // console.log(JSON.stringify(response.data));
+      // res.contentType('image/jpeg');
+      console.log('just set the content type');
+      const img = Buffer.from(response.data, 'base64');
 
-  requestBuffer(options, function (error, response, body) {
-    // Set response headers such that the browser/client can parse the raw binary image.
-    // body.length is the byte length of the binary image.
-    res.writeHead(200, {
-      'Content-Type': 'image/jpeg',
-      'Content-Length': body.length,
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': img.length,
+      });
+
+      res.end(img);
+      // res.end(response, 'binary');
+    })
+    .catch(function (error) {
+      console.log('Error pulled from Google API', error);
+      res.send(error);
     });
 
-    // Write out the raw binary buffer.
-    res.end(body);
-  });
+  // var options = {
+  //   method: 'GET',
+  //   url: ''
+  //   headers: {},
+  // };
+
+  // //
+
+  // // override requests to return raw binary Buffers instead of string data
+  // https: var requestBuffer = require('request').defaults({
+  //   encoding: null,
+  // });
+
+  // requestBuffer(options, function (error, response, body) {
+  //   // Set response headers such that the browser/client can parse the raw binary image.
+  //   // body.length is the byte length of the binary image.
+  //   // res.writeHead(200, {
+  //   //   'Content-Type': 'image/jpeg',
+  //   //   'Content-Length': body.length,
+  //   // });
+  //   console.log(error, response, body);
+  //   // Write out the raw binary buffer.
+  //   res.send(response);
+  // });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const morgan = require('morgan');
 var db = require('./models');
 
 require('dotenv').config();
-console.log(process.env);
+// console.log(process.env);
 
 const PORT = process.env.PORT || 3001;
 const appOrigin = process.env.APP_ORIGIN;
